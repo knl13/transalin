800351651ddbb24f1f056b1bc4518a343080bb60
knlabador <knl13>
2022-05-17 22:09:35 +0800

    progress

--- lib/classes/languages.dart
+++ lib/classes/languages.dart
@@ -3,13 +3,13 @@ import 'package:transalin/constants/app_language.dart';
 
 class Languages {
   static const chinese =
-      Language(icon: 'assets/images/china_flag.png', text: AppLanguage.chinese);
+      Language(icon: 'assets/icon/china_flag.png', text: AppLanguage.chinese);
   static const english =
-      Language(icon: 'assets/images/uk_flag.png', text: AppLanguage.english);
+      Language(icon: 'assets/icon/uk_flag.png', text: AppLanguage.english);
   static const filipino = Language(
-      icon: 'assets/images/philippines_flag.png', text: AppLanguage.filipino);
+      icon: 'assets/icon/philippines_flag.png', text: AppLanguage.filipino);
   static const korean = Language(
-      icon: 'assets/images/southkorea_flag.png', text: AppLanguage.korean);
+      icon: 'assets/icon/southkorea_flag.png', text: AppLanguage.korean);
 
   static const List<Language> languages = [chinese, english, filipino];
   // static const List<Language> languages = [chinese, english, filipino, korean];
